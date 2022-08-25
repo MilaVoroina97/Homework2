919ebf131335a1f86ba5665b6401c5c569077350
mila <milavoronina1997@mail.ru>
2022-08-26 04:32:36 +0500

    finally solve!!!

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -2,25 +2,22 @@
 
 Console.WriteLine("Введите число программистов:");
 int n = Convert.ToInt32(Console.ReadLine());
-if (n == 1 && n % 10 == 1)
+if (n % 10 == 1 && n % 100 != 11)
 {
     Console.WriteLine(n + " Программист");
 }
 else
 {
-    if (n == 11 && n % 100 == 11)
+    if(n % 10 == 2 && n %100 !=12 || n % 10 == 3 && n % 100 != 13 || n % 10 == 4 && n % 100 != 14)
+    {
+        Console.WriteLine(n + " Программиста");
+    }
+    else
     {
         Console.WriteLine(n + " Программистов"); 
     }
 }
+    
 
-if (n == 2 || n == 3 || n ==4 || n % 10 == 2 || n % 10 == 3 || n % 10 == 4)
-{
-    Console.WriteLine(n + " Программиста");
-}
- else
-{
-    Console.WriteLine(n + " Программистов"); 
-}
 
  
