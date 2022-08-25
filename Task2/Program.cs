8e25b64e56d5b68b089ab16ec1c89f69b9ed367c
mila <milavoronina1997@mail.ru>
2022-08-26 02:28:45 +0500

    Task2_solve

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+void ThirdNumber()
+{
+   System.Console.WriteLine("Введите целое число: ");
+   int x = Convert.ToInt32(Console.ReadLine());
+   string y = x.ToString();
+   if (x > 100)
+   {
+     Console.WriteLine(y[2]);
+   }
+   else
+   {
+    if (x < 100)
+    {
+        Console.WriteLine("третьей цифры нет");
+    }
+   }
+}
+ThirdNumber();
