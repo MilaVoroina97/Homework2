5663f4aeb09f8c018250b6cdb9c85bbeaa764530
mila <milavoronina1997@mail.ru>
2022-08-26 01:48:36 +0500

    Task1_solve

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+//void ShowSecondNumbwer()
+//{
+    //Console.WriteLine("Введите трехзначное число: ");
+    //int x = Convert.ToInt32(Console.ReadLine());
+    //if (x >= 100 && x < 1000)
+    //{
+        //int x1 = x /10 % 10 ;
+       // Console.WriteLine(x1);
+    //}
+    //else
+    //{
+        //Console.WriteLine("Вы ввели не трехзначное число,введите именно трехзначное ");
+    //}
+//}
+   
+//ShowSecondNumbwer();
+
+int ShowSecondNumbwer(int x)
+{   
+    int x1 = 0;
+    if (x >= 100 && x < 1000)
+    {
+        x1 = x /10 % 10 ;
+        Console.WriteLine(x1);   
+    }
+    else 
+    {  
+        Console.WriteLine("Вы ввели не трехзначное число,введите именно трехзначное ");
+    }
+    
+    return(x1);    
+}
+
+ShowSecondNumbwer(432);
