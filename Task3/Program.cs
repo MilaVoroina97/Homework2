f8ada9c2eb014c41dd4670db29810357f6866d26
mila <milavoronina1997@mail.ru>
2022-08-26 14:26:10 +0500

    task3

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -22,32 +22,3 @@ void DayWeek()
     }
 }
 DayWeek();
-
-
-// 2-ой вариант:
-
-void DayWeek()
-{
-    Console.WriteLine("Введите число, обозначающее день недели:");
-    int x = Convert.ToInt32(Console.ReadLine()) ;
-    if (x >= 1 && x <= 5)
-    {
-        Console.WriteLine("нет");
-    }
-    else
-    {
-        if (x == 6 || x == 7)
-        {
-            Console.WriteLine("да");
-        }
-        else
-        {
-            if (x == 0 || x < 0 || x > 7)
-            {
-                Console.WriteLine("Введите число от 1 до 7, обозначающее день недели ");
-            }
-        }
-    }
-    
-}
-DayWeek();
